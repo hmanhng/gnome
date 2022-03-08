94dc2932ecd253895fd45558560fcffbd95f108a
hmanhng <hmanhng@icloud.com>
2022-03-08 23:15:16 +0700

    up

--- dotfiles/.local/share/gnome-shell/extensions/system-monitor-next@paradoxxx.zero.gmail.com/extension.js
+++ dotfiles/.local/share/gnome-shell/extensions/system-monitor-next@paradoxxx.zero.gmail.com/extension.js
@@ -2418,7 +2418,7 @@ function enable() {
                 return true;
             });
     } else {
-        let panel = Main.panel._rightBox;
+        let panel = Main.panel._leftBox;
         StatusArea = Main.panel._statusArea;
         if (typeof (StatusArea) === 'undefined') {
             StatusArea = Main.panel.statusArea;
@@ -2463,7 +2463,7 @@ function enable() {
         Schema.connect('changed::background', (schema, key) => {
             Background = color_from_string(Schema.get_string(key));
         });
-        Main.panel._addToPanelBox('system-monitor', tray, 0, panel);
+        Main.panel._addToPanelBox('system-monitor', tray, 2, panel);
 
         // The spacing adds a distance between the graphs/text on the top bar
         let spacing = Schema.get_boolean('compact-display') ? '1' : '4';
