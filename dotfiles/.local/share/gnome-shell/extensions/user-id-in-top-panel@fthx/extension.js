9d6c2ba107f33a5006af4add63c2f2b305cce02f
hmanhng <hmanhng@icloud.com>
2022-06-01 20:18:18 +0700

    up

--- dotfiles/.local/share/gnome-shell/extensions/user-id-in-top-panel@fthx/extension.js
+++ dotfiles/.local/share/gnome-shell/extensions/user-id-in-top-panel@fthx/extension.js
@@ -1,13 +1,13 @@
 /*
   Add user id in top panel
-  (c) fthx 2021
+  (c) fthx 2022
   License: GPL v3
 */
 
 const Clutter = imports.gi.Clutter;
+const GLib = imports.gi.GLib;
 const St = imports.gi.St;
 const Main = imports.ui.main;
-const GLib = imports.gi.GLib;
 
 
 class Extension {
@@ -15,16 +15,18 @@ class Extension {
     }
 
     enable() {
-      this.id_label = new St.Label({text: GLib.get_user_name() + "@" + GLib.get_host_name(), y_align: Clutter.ActorAlign.CENTER, style_class: "user-label"});
+      this.id_label = new St.Label({text: GLib.get_real_name() + " :: " + GLib.get_user_name() + "@" + GLib.get_host_name(),
+                                    y_align: Clutter.ActorAlign.CENTER, style_class: "user-label"});
       this.aggregate_menu = Main.panel.statusArea["aggregateMenu"];
       this.aggregate_menu._indicators.insert_child_above(this.id_label, this.aggregate_menu._power);
     }
 
     disable() {
-		  this.aggregate_menu._indicators.remove_child(this.id_label); 
+      this.aggregate_menu._indicators.remove_child(this.id_label);
+      this.id_label = null;
     }
 }
 
 function init() {
-	return new Extension();
+  return new Extension();
 }
